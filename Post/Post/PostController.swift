3cc6b808925dfdd2559270d5004d649ebbc47cf5
AustinDev16 <austin.blaser@gmail.com>
2016-08-30 15:59:14 -0600

    Added a post controller delegate, implemented sorting of posts by timestamp, and added an initializer to fetch posts. Added a didSet property on posts to call a delegate method

--- Post/Post/PostController.swift
+++ Post/Post/PostController.swift
@@ -7,24 +7,53 @@
 //
 
 import Foundation
+protocol PostControllerDelegate {
+    func postsUpdated(posts: [Post]?)
+}
+
 class PostController {
     
-    static let baseURL = "https://devmtn-post.firebaseio.com/"
+    var delegate: PostControllerDelegate?
     
-    static func fetchPosts(completion: ([Post]) -> Void) {
-        guard let endPoint = NSURL(string: self.baseURL + "posts.json") else {completion([]); return}
+    var posts: [Post]? {
+        didSet{
+            if let delegate = delegate {
+                delegate.postsUpdated(self.posts)
+            }
+        }
+    }
+    
+    init(){
+        fetchPosts { (posts) in
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.posts = posts
+            })
+        }
+    }
+    
+    
+    
+    
+    func fetchPosts(completion: ([Post]) -> Void) {
+        let baseURL = "https://devmtn-post.firebaseio.com/"
+        guard let endPoint = NSURL(string: baseURL + "posts.json") else {completion([]); return}
         
         
         NetworkController.performRequestForURL(endPoint, httpMethod: .Get) { (data, error) in
             
             guard let data = data,
-            jsonAnyObject = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)),
+                jsonAnyObject = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)),
                 jsonDictionary = jsonAnyObject as? [String: [String: AnyObject]] else { completion([]); return}
             
             
             let arrayOfPosts: [Post] = jsonDictionary.flatMap({Post(dictionary: $0.1, identifier: $0.0)})
             
-            completion(arrayOfPosts)
+            // sort by timestamp
+            
+            let sortedArrayOfPosts = arrayOfPosts.sort {  $0.timestamp < $1.timestamp   }
+            
+            completion(sortedArrayOfPosts)
             
             
         }
