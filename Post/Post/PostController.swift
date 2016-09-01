f51d9f402963ac874174b037b22d0fba70076e93
AustinDev16 <austin.blaser@gmail.com>
2016-09-01 15:05:14 -0600

    Added PostController: add post and fetch post functions

---
+++ Post/Post/PostController.swift
@@ -0,0 +1,64 @@
+//
+//  PostController.swift
+//  Post
+//
+//  Created by Austin Blaser on 9/1/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import Foundation
+class PostController{
+    
+    static let sharedController = PostController()
+    
+    let baseURL = NSURL(string: "https://devmtn-post.firebaseio.com/posts")
+    
+    
+    var fetchedPosts: [Post] = [] {
+        didSet{
+            // delegate method to reload any uiview elements
+        }
+    }
+    
+    func addPost(username: String, message: String, completion: () -> Void){
+        
+        let newPost = Post(username: username, message: message)
+        
+        guard let url = baseURL?.URLByAppendingPathComponent(newPost.identifier.UUIDString).URLByAppendingPathExtension("json") else { return}
+        
+        NetworkController.performRequestForURL(url, httpMethod: .Put, body: newPost.jsonData) { (data, error) in
+            
+            if error != nil {
+                print("Error: \(error?.localizedDescription)")
+            } else {
+                print("Success: \(error?.localizedDescription)")
+            }
+            
+            completion()
+        }
+        
+        
+    }
+    
+    func fetchPosts(completion: (posts: [Post]) -> Void){
+        guard let url = baseURL?.URLByAppendingPathExtension("json") else {return}
+        
+        
+        NetworkController.performRequestForURL(url, httpMethod: .Get) { (data, error) in
+            
+            guard let data = data,
+                let jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)) as? [String: [String: AnyObject]] else { completion(posts: []); return}
+            
+            let fetchedPosts = jsonDictionary.flatMap { Post(dictionary: $0.1, identifier: $0.0) }
+            
+            completion(posts: fetchedPosts)
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+}
